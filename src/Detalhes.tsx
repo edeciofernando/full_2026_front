3a8c4bfcf76327d5aaeb55beeb190103d87d672a
Edécio Fernando Iepsen <edeciofernando@gmail.com>
2026-04-02 09:56:50 -0300

    Cadastro de Propostas, Minhas Propostas e Cad. Cliente

--- src/Detalhes.tsx
+++ src/Detalhes.tsx
@@ -2,14 +2,22 @@ import type { CarroType } from "./utils/CarroType"
 import { useParams } from "react-router-dom"
 import { useEffect, useState } from "react"
 import { useClienteStore } from "./context/ClienteContext"
+import { useForm } from "react-hook-form"
+import { toast } from 'sonner'
 
 const apiUrl = import.meta.env.VITE_API_URL
+
+type Inputs = {
+  descricao: string
+}
 
 export default function Detalhes() {
   const params = useParams()
 
   const [carro, setCarro] = useState<CarroType>()
   const { cliente } = useClienteStore()
+
+  const { register, handleSubmit, reset } = useForm<Inputs>()
 
   useEffect(() => {
     async function buscaDados() {
@@ -20,6 +28,28 @@ export default function Detalhes() {
     }
     buscaDados()
   }, [])
+
+  async function enviaProposta(data: Inputs) {
+
+    const response = await fetch(`${apiUrl}/propostas`, {
+      headers: {
+        "Content-Type": "application/json"
+      },
+      method: "POST",
+      body: JSON.stringify({
+        clienteId: cliente.id,
+        carroId: Number(params.carroId),
+        descricao: data.descricao
+      })
+    })
+
+    if (response.status == 201) {
+      toast.success("Obrigado. Sua proposta foi enviada. Aguarde retorno")
+      reset()
+    } else {
+      toast.error("Erro... Não foi possível enviar sua proposta")
+    }
+  }
 
   return (
     <>
@@ -44,11 +74,13 @@ export default function Detalhes() {
             <>
               <h3 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
                 🙂Você pode fazer uma Proposta para este veículo!</h3>
-              <form>
+              <form onSubmit={handleSubmit(enviaProposta)}>
                 <input type="text" className="mb-2 mt-4 bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-not-allowed dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" value={`${cliente.nome} (${cliente.email})`} disabled readOnly />
                 <textarea id="message" className="mb-2 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="Descreva a sua proposta"
-                  required></textarea>
+                  required
+                  {...register("descricao")}>
+                </textarea>
                 <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Enviar Proposta</button>
               </form>
             </>
@@ -57,8 +89,6 @@ export default function Detalhes() {
               😎Gostou? Identifique-se e faça uma Proposta!
             </h2>
           }
-
-
         </div>
       </section>
     </>
